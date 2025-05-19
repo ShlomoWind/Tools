76164c573603cfe48f7c21ae993f2a4c00724d2f
Shlomo <sh.z.wind@gmail.com>
2025-05-19 17:15:30 +0300

    create class NailGun

---
+++ Tools/NailGun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class NailGun : Tool
+{
+    public NailGun (int weight) : base("NailGun", weight)
+    {
+    }
+    public override void Describe()
+    {
+        Console.WriteLine($"\ntool: {this.name} \ndescribe: is an electric, pneumatic, or cordless tool used to quickly and forcefully drive nails into surfaces.\nweight: {this.weight}");
+    }
+    public override void Use()
+    {
+        Console.WriteLine("use: Wood construction work");
+    }
+}
