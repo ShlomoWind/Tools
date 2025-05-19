54f6f125595fe86a847d9401bc44668236af0899
Shlomo <sh.z.wind@gmail.com>
2025-05-19 15:53:13 +0300

    create class Wrench

---
+++ Tools/Wrench.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Wrench : Tool
+{
+    public Wrench(int weight) : base("Wrench", weight)
+    {
+    }
+    public override void Describe()
+    {
+        Console.WriteLine($"tool: {this.name} \ndescribe: is used to tighten or loosen bolts and nuts..\nweight: {this.weight}");
+    }
+    public override void Use()
+    {
+        Console.WriteLine("use: Inserting nails");
+    }
+}
+}
