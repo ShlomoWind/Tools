14c6f741c2729cf982caf3b45dbf010801aeb230
Shlomo <sh.z.wind@gmail.com>
2025-05-19 17:11:40 +0300

    create class WireStripper

---
+++ Tools/WireStripper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class WireStripper : Tool
+{
+    public WireStripper(int weight) : base("WireStripper", weight)
+    {
+    }
+    public override void Describe()
+    {
+        Console.WriteLine($"\ntool: {this.name} \ndescribe: is a tool used to remove the insulation from the outside of electrical wires.\nweight: {this.weight}");
+    }
+    public override void Use()
+    {
+        Console.WriteLine("use: Stripping insulation from electrical wires");
+    }
+}
